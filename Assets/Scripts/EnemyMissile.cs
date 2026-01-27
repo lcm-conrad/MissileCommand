5e5b7b953914fb7e5087e890375329fa0b014f57
lcm-conrad <sagrado.luisconrad@smctagum.edu.ph>
2026-01-27 21:27:50 +0800

    missile collisions

---
+++ Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject ExplosionPrefab;
+
+    GameObject[] Base;
+    Vector3 target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Base = GameObject.FindGameObjectsWithTag("Base");
+        target = Base[Random.Range(0, Base.Length)].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Base"))
+        {
+            MissileExplode();
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Explosion"))
+        {
+            MissileExplode();
+            Destroy(collision.gameObject);
+
+        } 
+    }
+
+        private void MissileExplode()
+        {
+            Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+}
+
--- Assets/Scripts/EnemyMissileSpawner.cs
+++ Assets/Scripts/EnemyMissileSpawner.cs
@@ -1,18 +1,47 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyMissileSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyMissilePrefab;
+    [SerializeField] private float yPadding = 0.5f;
+    private float minX, maxX;
+    public int missilesToSpawnThisRound = 10;
+    public float timeBetweenSpawns = 0.5f;
 
+    float randomX;
+    float yValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        minX = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).x;
+        maxX = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0)).x;
+
+        randomX = Random.Range(minX, maxX);
+        yValue = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+        Instantiate(enemyMissilePrefab, new Vector3(randomX, yValue + yPadding, 0), Quaternion.identity);
         
+
+        StartCoroutine(SpawnMissiles());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator SpawnMissiles()
+    {
+
+        while (missilesToSpawnThisRound > 0)
+        {
+
+            Instantiate(enemyMissilePrefab, new Vector3(randomX, yValue + yPadding, 0), Quaternion.identity);
+            missilesToSpawnThisRound--;
+
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
     }
 }
